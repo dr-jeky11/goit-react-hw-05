466037fe3531bd33433d6bafed60cdc3119188d0
dr-jeky11 <tyrsyna@gmail.com>
2024-08-02 10:01:17 +0200

    movie cast additional

---
+++ src/components/MovieCast/MovieCas.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { getCastInfo } from "../../moviesAPI/movies-api";
+import { createActorUrl } from "../../helpers/createImageUrl.js";
+
+import s from "./MovieCast.module.css";
+import Error from "../Error/Error.jsx";
+import Loader from "../Loader/Loader.jsx";
+
+export default function MovieCast() {
+  const [cast, setCast] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    if (!movieId) return;
+
+    const handleCastFetch = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+
+        const data = await getCastInfo(movieId);
+        setCast(data.cast.slice(0, 5));
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    handleCastFetch();
+  }, [movieId]);
+
+  return (
+    <div className={s.castSection}>
+      {cast.length > 0 && (
+        <ul className={s.castList}>
+          {cast.map((actor) => {
+            return (
+              <li key={actor.id} className={s.actorItem}>
+                <img
+                  src={createActorUrl(actor.profile_path, 200)}
+                  alt={`${actor.name} photo`}
+                  className={s.actorPhoto}
+                />
+                <p>{actor.name}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      {cast.length < 1 && !loading && <p>No data</p>}
+      {error && <Error />}
+      {loading && <Loader />}
+    </div>
+  );
+}
